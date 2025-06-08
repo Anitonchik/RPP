c66c3995d811f2e1c805445af5e9e848982fcb6f
Anitonchik <149405844+Anitonchik@users.noreply.github.com>
2025-06-09 00:47:32 +0400

    3

---
+++ DataModelsTests/OrderProductDataModelTests.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RPP.DataModels;
+
+namespace RPP_Tests.DataModelsTests;
+
+[TestFixture]
+internal class OrderProductDataModelTests
+{
+    [Test]
+    public void OrderIdIsNullOrEmptyTest()
+    {
+        var order = CreateDataModel(Guid.NewGuid().ToString(), null, 1);
+        Assert.That(() => order.Validate(), Throws.TypeOf<ValidationException>());
+        order = CreateDataModel( Guid.NewGuid().ToString(), string.Empty, 1);
+        Assert.That(() => order.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void OrderIdIsNotGuidTest()
+    {
+        var order = CreateDataModel(Guid.NewGuid().ToString(), "id", 1);
+        Assert.That(() => order.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void ProductIdIsNullOrEmptyTest()
+    {
+        var product = CreateDataModel(null, Guid.NewGuid().ToString(), 1);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+        product = CreateDataModel(string.Empty, Guid.NewGuid().ToString(), 1);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void ProductIdIsNotGuidTest()
+    {
+        var product = CreateDataModel("id", Guid.NewGuid().ToString(), 1);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void CountNotCorrectTest()
+    {
+        var order = CreateDataModel(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), -10);
+        Assert.That(() => order.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void AllFieldsIsCorrectTest()
+    {
+        var orderId = Guid.NewGuid().ToString();
+        var productId = Guid.NewGuid().ToString();
+        var count = 10;
+        var orderProduct = CreateDataModel(orderId, productId, count);
+        Assert.That(() => orderProduct.Validate(), Throws.Nothing);
+        Assert.Multiple(() =>
+        {
+            Assert.That(orderProduct.OrderId, Is.EqualTo(orderId));
+            Assert.That(orderProduct.ProductId, Is.EqualTo(productId));
+            Assert.That(orderProduct.Count, Is.EqualTo(count));
+        });
+    }
+
+    private static OrderProductDataModel CreateDataModel(string? orderId, string? productId, int count) =>
+        new(orderId, productId, count);
+}
---
+++ DataModelsTests/ProductDataModelTests.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RPP.DataModels;
+using RPP.Enums;
+
+namespace RPP_Tests.DataModelsTests;
+
+[TestFixture]
+internal class ProductDataModelTests
+{
+    [Test]
+    public void IdIsNullOrEmptyTest()
+    {
+        var product = CreateDataModel(null, "name", ProductType.Cloth, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+        product = CreateDataModel(string.Empty, "name", ProductType.Cloth, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void IdIsNotGuidTest()
+    {
+        var product = CreateDataModel("id", "name", ProductType.Cloth, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void ProductNameIsEmptyTest()
+    {
+        var product = CreateDataModel(Guid.NewGuid().ToString(), null, ProductType.Cloth, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+        product = CreateDataModel(Guid.NewGuid().ToString(), string.Empty, ProductType.Cloth, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void ProductTypeIsNoneTest()
+    {
+        var product = CreateDataModel(Guid.NewGuid().ToString(), null, ProductType.None, 10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void PriceIsLessOrZeroTest()
+    {
+        var product = CreateDataModel(Guid.NewGuid().ToString(), "name",  ProductType.Cloth, 0, false);
+        Assert.That(() => product.Validate(),
+        Throws.TypeOf<ValidationException>());
+        product = CreateDataModel(Guid.NewGuid().ToString(), "name", ProductType.Cloth, -10, false);
+        Assert.That(() => product.Validate(), Throws.TypeOf<ValidationException>());
+    }
+
+    [Test]
+    public void AllFieldsIsCorrectTest()
+    {
+        var productId = Guid.NewGuid().ToString();
+        var productName = "name";
+        var productType = ProductType.Cloth;
+        var productPrice = 10;
+        var productIsDelete = false;
+        var product = CreateDataModel(productId, productName, productType, productPrice, productIsDelete);
+        Assert.That(() => product.Validate(), Throws.Nothing);
+        Assert.Multiple(() =>
+        {
+            Assert.That(product.Id, Is.EqualTo(productId));
+            Assert.That(product.ProductName, Is.EqualTo(productName));
+            Assert.That(product.ProductType, Is.EqualTo(productType));
+            Assert.That(product.Price, Is.EqualTo(productPrice));
+            Assert.That(product.IsDeleted, Is.EqualTo(productIsDelete));
+        });
+    }
+    private static ProductDataModel CreateDataModel(string? id, string? productName, ProductType productType,  
+        double price, bool isDeleted) => new(id, productName, productType, price, isDeleted);
+}
